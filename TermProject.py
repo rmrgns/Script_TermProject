9a2fe779fe8f23c43a67ab7bcf081056fe8b554d
Jung Geuk Hun <111038432+rmrgns@users.noreply.github.com>
2023-05-23 15:46:27 +0900

    Update xml read
    공공데이터포털에서 xml를 읽어오는 코드 구현

--- TermProject.py
+++ TermProject.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import font
+import tkinter.ttk
+import xml.etree.ElementTree as ET
 import requests
 
 ServiceKey = 'dZcoKqxJ0w46SNHY9aMe4zgyOynLtTE0cL4fm9OOQ7oboRaunGQ09BLwKlqx1nwpH8hDfNRVFDrOOsH2Tv5jEg=='
@@ -8,10 +10,31 @@ url2 = 'https://apis.data.go.kr/1400377/forestPoint/forestPointListEmdSearch'
 # url3 = 'https://apis.data.go.kr/1400000/forestStusService/getfirestatsservice'
 
 queryParams = {'serviceKey': ServiceKey, 'pageNo': '1', 'numOfRows': '10'}
+response = requests.get(url1, params=queryParams)
+root = ET.fromstring(response.text)
 
+header1 = ['mntiname', 'mntiadd', 'mntihigh', 'mntiadmin', 'mntiadminnum']
 
 class MainGUI:
     def SearchName(self):
+        for i, col_name in enumerate(header1):
+            label = tkinter.Label(self.frame2, text=col_name, font=("Helvetica", 14, "bold"))
+            label.grid(row=0, column=i)
+
+        self.row_count = 1
+        for item in root.iter("item"):
+            self.mntiname = item.findtext("mntiname")
+            self.mntiadd = item.findtext("mntiadd")
+            self.mntihigh = item.findtext("mntihigh")
+            self.mntiadmin = item.findtext("mntiadmin")
+            self.mntiadminnum = item.findtext("mntiadminnum")
+
+            self.data = [self.mntiname, self.mntiadd, self.mntihigh, self.mntiadmin, self.mntiadminnum]
+            for i, value in enumerate(self.data):
+                label = tkinter.Label(self.frame2, text=value, font=("Helvetica", 12))
+                label.grid(row=self.row_count, column=i)
+
+            self.row_count += 1
         pass
 
     def SearchLocation(self):
@@ -24,7 +47,10 @@ class MainGUI:
         # window 생성 및 타이틀 설정
         window = Tk()
         window.title('등산 알리미')
-
+        self.frame2 = tkinter.Frame(window)
+        self.frame2.pack(side='right')
+        #notebook = tkinter.ttk.Notebook(window, width=800, height=600)
+        #notebook.pack()
         # Main Screen
         # 제목
         self.AppNameFont = font.Font(window, size=32, weight='bold')
@@ -36,11 +62,15 @@ class MainGUI:
         self.MainImageLb = Label(window, image=self.MainImage)
         self.MainImageLb.pack()
 
-        frame = Frame(window)
-        frame.pack()
-        self.SearchNameBtn = Button(frame, text='산 이름 검색', command=self.SearchName)
-        self.SearchLocationBtn = Button(frame, text='지역 이름 검색', command=self.SearchLocation)
-        self.LikeListBtn = Button(frame, text='즐겨찾기', command=self.LikeList)
+        self.frame = Frame(window)
+        self.frame.pack()
+        #frameMtnName = Frame(window)
+        #frameMtnName.pack(side='right')
+        #notebook.add(frameMtnName, text='산 이름 검색')
+        self.SearchNameBtn = Button(self.frame, text='산 이름 검색', command=self.SearchName)
+        #Label(frameMtnName, text="페이지1의 내용", fg='red', font='helvetica 48').pack()
+        self.SearchLocationBtn = Button(self.frame, text='지역 이름 검색', command=self.SearchLocation)
+        self.LikeListBtn = Button(self.frame, text='즐겨찾기', command=self.LikeList)
 
         self.SearchNameBtn.pack()
         self.SearchLocationBtn.pack()
