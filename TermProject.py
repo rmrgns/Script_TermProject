7c67b751eb2c50dedcc72ea0ef703ced725a6a73
wjdrmrgns <rmrgns@naver.com>
2023-06-12 23:56:35 +0900

    산 검색리스트 완성

--- TermProject.py
+++ TermProject.py
@@ -12,12 +12,13 @@ url2 = 'https://apis.data.go.kr/1400377/forestPoint/forestPointListEmdSearch'
 
 
 
-header1 = ['이름', '주소', '높이', '소재지', '소재지 전화번호']
-nameList = []
+header1 = ['이름', '주소', '높이', '소재지', '소재지 전화번호', '상세정보']
+
 
 class MainGUI:
     def SearchName(self):
-        self.EmptySearchName()
+        self.SearchListBox.delete(0, END)
+        self.MntList = []
         self.name = self.SearchInput.get()
         queryParams = {'serviceKey': ServiceKey, 'searchWrd': self.name, 'numOfRows': 3368}
         #queryParams = {'serviceKey': ServiceKey, 'numOfRows': 3368}
@@ -33,20 +34,45 @@ class MainGUI:
             self.mntihigh = item.findtext("mntihigh")
             self.mntiadmin = item.findtext("mntiadmin")
             self.mntiadminnum = item.findtext("mntiadminnum")
-            #self.mntidetails = item.findtext("mntidetails")
-            self.mntilistno = item.findtext("mntilistno")
-            self.data = [self.mntiname, self.mntiadd, self.mntihigh, self.mntiadmin, self.mntiadminnum]
-            #self.data = [self.mntiname, self.mntiadd, self.mntihigh, self.mntiadmin, self.mntiadminnum, self.mntidetails]
-            for i, value in enumerate(self.data):
-                label = tkinter.Label(self.frame2, text=value, font=("Helvetica", 12))
-                label.grid(row=self.row_count, column=i)
-            #button = tkinter.Button(self.frame2, text='숲길 정보', font=("Helvetica", 12), command=self.SearchMountainLoad(self.mntilistno))
-            #button.grid(row=self.row_count, column=5)
+            self.mntidetails = item.findtext("mntidetails")
+            self.mntitop = item.findtext("mntitop")
+            self.SearchListBox.insert(self.row_count, self.mntiname)
+            self.MntList.append([self.mntiname, self.mntiadd, self.mntihigh, self.mntiadmin, self.mntiadminnum, self.mntidetails, self.mntitop])
             self.row_count += 1
-    def EmptySearchName(self):
-        myList = self.frame2.grid_slaves()
-        for i in myList:
-            i.destroy()
+
+
+    def SearchMnt(self):
+        self.RenderText.configure(state='normal')
+        self.RenderText.delete(0.0, END)
+        self.LbIndex = self.SearchListBox.curselection()[0]
+
+        mnti = self.MntList[self.LbIndex]
+
+
+        self.RenderText.insert(INSERT, '이름: ')
+        self.RenderText.insert(INSERT, mnti[0])
+        self.RenderText.insert(INSERT, '\n')
+        self.RenderText.insert(INSERT, '주소: ')
+        self.RenderText.insert(INSERT, mnti[1])
+        self.RenderText.insert(INSERT, '\n')
+        self.RenderText.insert(INSERT, '높이: ')
+        self.RenderText.insert(INSERT, mnti[2])
+        self.RenderText.insert(INSERT, '\n')
+        self.RenderText.insert(INSERT, '소재지: ')
+        self.RenderText.insert(INSERT, mnti[3])
+        self.RenderText.insert(INSERT, '\n')
+        self.RenderText.insert(INSERT, '소재지 전화번호')
+        self.RenderText.insert(INSERT, mnti[4])
+        self.RenderText.insert(INSERT, '\n')
+        self.RenderText.insert(INSERT, '상세정보: ')
+        self.RenderText.insert(INSERT, mnti[5])
+        self.RenderText.insert(INSERT, '\n')
+        self.RenderText.insert(INSERT, '봉우리 정보: ')
+        self.RenderText.insert(INSERT, mnti[6])
+        self.RenderText.insert(INSERT, '\n')
+
+        self.RenderText.configure(state='disabled')
+
 
     def SearchMountainLoad(self, listno):
         queryParams2 = {'serviceKey': ServiceKey, 'mntiListNo': listno}
@@ -63,7 +89,6 @@ class MainGUI:
 
 
     def SearchLocation(self):
-        self.EmptySearchName()
         self.location = self.LocationInput.get()
         # queryParams = {'serviceKey': ServiceKey, 'searchWrd': self.name, 'numOfRows': 3368}
         queryParams = {'serviceKey': ServiceKey, 'numOfRows': 3368}
@@ -111,13 +136,29 @@ class MainGUI:
 
         ListBoxScrollbar.config(command=self.SearchListBox.yview)
 
+    def InitRenderText(self):
+        RenderTextScrollbar = Scrollbar(self.window)
+        RenderTextScrollbar.pack()
+        RenderTextScrollbar.place(x=625,y=150)
+
+        self.RenderText = Text(self.window, width=45, height=30, borderwidth=10, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+        self.RenderText.pack()
+        self.RenderText.place(x=420, y=180)
+        RenderTextScrollbar.config(command=self.RenderText.yview)
+        RenderTextScrollbar.pack(side=RIGHT, fill=BOTH)
+
+        self.RenderText.configure(state='disabled')
+
     def __init__(self):
         self.name = ""
         self.SearchLabel = []
         # window 생성 및 타이틀 설정
         self.window = Tk()
         self.window.title('등산 알리미')
-        self.window.geometry('800x600')
+        self.window.geometry('1200x600')
+
+        self.MntList = []
+
         self.frame2 = tkinter.Frame(self.window)
         self.frame2.pack()
         self.frame2.place(x=200, y=0)
@@ -145,7 +186,6 @@ class MainGUI:
         #frameMtnName.pack(side='right')
         #notebook.add(frameMtnName, text='산 이름 검색')
         self.SearchNameBtn = Button(self.frame, text='산 이름 검색', command=self.SearchName)
-        #Label(frameMtnName, text="페이지1의 내용", fg='red', font='helvetica 48').pack()
         self.SearchLocationBtn = Button(self.frame, text='지역 이름 검색', command=self.SearchLocation)
         self.LikeListBtn = Button(self.frame, text='즐겨찾기', command=self.LikeList)
         self.SearchInput = Entry(self.frame)
@@ -160,11 +200,14 @@ class MainGUI:
         #for i, col_name in enumerate(header1):
         #    label = tkinter.Label(self.frame2, text=col_name, font=("Helvetica", 14, "bold"))
         #    label.grid(row=0, column=i)
-        self.label = tkinter.Label(self.window, text='이름', font=("Helvetica", 14, "bold"))
+        self.label = Label(self.window, text='이름', font=("Helvetica", 14, "bold"))
         self.label.pack()
         self.label.place(x=500, y=10)
-
+        self.SearchListboxBtn = Button(self.window, font=60, text='검색', command=self.SearchMnt)
+        self.SearchListboxBtn.pack()
+        self.SearchListboxBtn.place(x=700, y=100)
         self.InitSearchListBox()
+        self.InitRenderText()
 
         self.window.mainloop()
 
